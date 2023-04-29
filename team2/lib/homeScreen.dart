e0b5c73c7248df4090f697e6927e3d6498b1b157
arun <cvaruncv1998@gmail.com>
2023-04-29 11:00:13 +0530

    home screen ui

---
+++ team2/lib/homeScreen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  void _onItemTapped(int index) {
+    if (index == 0) {
+      print('Take a picture');
+    } else if (index == 1) {
+      print('choose from gallery');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Team 2 '),),
+      body: ListView.separated(
+          itemBuilder: ((context, index) {
+            return Row(
+              children: [
+                Expanded(
+                    child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.network(
+                      'https://images.pexels.com/photos/1213294/pexels-photo-1213294.jpeg?auto=compress&cs=tinysrgb&w=600'),
+                )),
+                Expanded(
+                    child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.network(
+                      'https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg?auto=compress&cs=tinysrgb&w=600'),
+                ))
+              ],
+            );
+          }),
+          separatorBuilder: (context, int index) => const Divider(),
+          itemCount: 20),
+      bottomSheet: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera),
+            label: 'Take a picture',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.image),
+            label: 'Choose from gallery',
+          ),
+        ],
+        unselectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
